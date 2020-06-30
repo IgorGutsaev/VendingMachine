6aeca6148343cd40d072ac68e3dde796ac6eac30
Alexey Krytikov <alexey.krytikov@filuet.ru>
2020-06-30 14:58:28 +0300

    add begin cashless abstractions

---
+++ Payment/Cashless/Filuet.ASC.Kiosk.OnBoard.Cashless.Abstractions/Interfaces/ICardDeviceAdapter.cs
@@ -0,0 +1,33 @@
+﻿using Filuet.ASC.Kiosk.OnBoard.Cashless.Abstractions.Events;
+using Filuet.Utils.Common.Business;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Filuet.ASC.Kiosk.OnBoard.Cashless.Abstractions
+{
+    public interface ICardDeviceAdapter
+    {
+        void Test();
+
+        void StopDevice();
+
+        void StopPayment();
+
+        void StartPayment(Money money);
+
+        void ReturnPayment(Money money);
+
+        event EventHandler<TestCardEventArgs> OnTest;
+
+        event EventHandler<StopCardEventArgs> OnStopDevice;
+
+        event EventHandler<StopCardEventArgs> OnStopPayment;
+
+        event EventHandler<CardEventArgs> OnStartPayment;
+
+        event EventHandler<CardEventArgs> OnFinishedPayment;
+
+        event EventHandler<CardEventArgs> OnReturnPayment;
+    }
+}
---
+++ Payment/Cashless/Filuet.ASC.Kiosk.OnBoard.Cashless.Abstractions/Interfaces/ICardPaymentService.cs
@@ -0,0 +1,32 @@
+﻿using Filuet.ASC.Kiosk.OnBoard.Cashless.Abstractions.Events;
+using Filuet.ASC.Kiosk.OnBoard.Storage.Abstractions;
+using Filuet.Utils.Common.Business;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Filuet.ASC.Kiosk.OnBoard.Cashless.Abstractions
+{
+    public interface ICardPaymentService
+    {
+        void AddCardDevice(ICardDeviceAdapter cardDevice);
+
+        void RemoveCardDevice();
+
+        void StopPayment();
+
+        void StartPayment(Money money);
+
+        void ReturnPayment(Money money);
+
+        event EventHandler<StopCardEventArgs> OnGoodStop;
+        event EventHandler<StopCardEventArgs> OnBadStop;
+
+        event EventHandler<CardEventArgs> OnGoodPayment;
+        event EventHandler<CardEventArgs> OnBadPayment;
+
+        event EventHandler<CardEventArgs> OnGoodReturnPayment;
+        event EventHandler<CardEventArgs> OnBadReturnPayment;
+    }
+}
