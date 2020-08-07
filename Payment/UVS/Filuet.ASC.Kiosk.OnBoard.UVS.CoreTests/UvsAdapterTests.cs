ea7e8bb2fa569a207f51eeacf36b95c1c82bfc18
Alexey Krytikov <alexey.krytikov@filuet.ru>
2020-08-07 17:10:48 +0300

    add tests

---
+++ Payment/UVS/Filuet.ASC.Kiosk.OnBoard.UVS.CoreTests/UvsAdapterTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Filuet.ASC.Kiosk.OnBoard.UVS.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Filuet.ASC.Kiosk.OnBoard.UVS.Core.Tests
+{
+    [TestClass()]
+    public class UvsAdapterTests
+    {
+        [TestMethod()]
+        public void GetOrderPaymentResultTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
